3faafb810f6eced54d3f75ba7203b939e673d72b
apbs <aparnabs.prasad@gmail.com>
2020-03-11 21:01:30 +0100

    clean up

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,19 +3,23 @@ import { Provider } from 'react-redux';
 import configureStore from '../../store/createStore';
 import HomePage from '../HomePage/HomePage';
 import { Container } from 'react-bootstrap';
-
-const styles = {
+import styled from 'styled-components';
+/*const styles = {
     container: {
         padding: '0 15px',
         height: '100%'
     },
-}
+}*/
+const StyledContainer = styled(Container)`
+padding: '0 15px';
+height: '100%';
+`
 const App = () => {
     const store = configureStore();
     return (<Provider data-test-id='app-component-id' store={store}>
-        <Container fluid style={styles.container}>
+        <StyledContainer fluid>
             <HomePage />
-        </Container>
+        </StyledContainer>
     </Provider>
     );
 }
